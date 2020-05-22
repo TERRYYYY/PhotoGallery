1585713cec4a611785bfb0b131684e4085787ba7
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-22 18:01:45 +0300

    creating template to display the welcome page

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
 # Create your views here.
 
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+def index(request):
+    return render(request, 'index.html')
