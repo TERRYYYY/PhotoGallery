55ff2e96908833cb93aca3d673493e778a3e7dff
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-25 15:20:55 +0300

    testing django apps

--- photos/models.py
+++ photos/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-class Image(models.Model):
-    image_name = models.CharField(max_length=255)
-    description = models.TextField()
-    location = models.ForeignKey("Location", on_delete=models.CASCADE)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.image_name
-    class Meta:
-        ordering = ['image_name']
-
 class Location(models.Model):
     location = models.CharField(max_length=255, blank=True)
 
@@ -23,9 +10,23 @@ class Location(models.Model):
         ordering = ['location']
 
 class Category(models.Model):
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.category
     class Meta:
         ordering = ['category']
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=255)
+    description = models.TextField()
+    image_file = models.ImageField(upload_to='images/', default='')
+    location = models.ForeignKey("Location", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_name
+    class Meta:
+        ordering = ['image_name']
+
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.new_location = Location(location = 'Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_location,Location))
+
+    def test_save_method(self):
+        self.new_location.save()
+        location = Location.objects.all()
+        self.assertTrue(len(location) > 0)
+
+class CategoryTestClass(TestCase):
+    
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.new_category = Category(category = 'Food')
+    
+    def test_save(self):
+        self.new_category.save()
+
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+class ImageTestClass(TestCase):
+    
+    def SetUp(self):
+        self.image = Image(title = 'BB', description ='Test Description', location = self.new_location, category = self.new_category, image ='/media/images/ilnur-kalimullin-kP1AxmCyEXM-unsplash2.jpg')
+
+        self.image.save_image()
+
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+        Image.del_photo(self.image.id)        
+
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
