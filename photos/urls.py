6bee9c606493c3f8cf65c697bd2fb83182db9dc6
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-25 17:43:14 +0300

    creating a special route to display a single image

--- photos/urls.py
+++ photos/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    url(r'^search/', views.search_images, name='search_images')
-    # url('^$',views.index,name = 'index'),
+    url(r'^search/', views.search_images, name='search_images'),
+    url(r'^image/(\d+)', views.get_image, name='image_results'),
+    url(r'^location/(?P<location>\w{0,50})/', views.location, name='location_results'),
+    url(r'^category/(?P<category>\w{0,50})/', views.category, name='category_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -26,4 +26,29 @@ def search_images(request):
 
   else:
     message = "Invalid Search"
-    return render(request, 'all-photos/search.html', {"message": message, "locations": locations})  
+    return render(request, 'all-photos/search.html', {"message": message, "locations": locations}) 
+
+def get_image(request, id):
+  locations = Location.get_location()
+  try:
+    image = Image.objects.get(pk=id)
+    print(image)
+
+  except ObjectDoesNotExist:
+    raise Http404()
+
+  return render(request, "images.html", {"image": image, "locations": locations})
+
+def location(request, location ): 
+    images = Image.search_by_location(location)
+    locations = Location.get_location()
+    message = f"{location}"
+    return render(request, 'locations.html', {"message":message, "images":images, "locations":locations})
+
+
+def category(request, category):
+    images = Image.get_by_category(category)
+    locations = Location.get_location()
+    message = f"{category}"
+    return render(request, 'category.html', {"message":message, "images":images, "locations":locations})
+ 
