a88aa7b0722dd4ab1e88b4904a329c13dcb99be4
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-25 10:29:04 +0300

    adding database relationships

--- photos/models.py
+++ photos/models.py
@@ -5,8 +5,27 @@ from django.db import models
 class Image(models.Model):
     image_name = models.CharField(max_length=255)
     description = models.TextField()
+    location = models.ForeignKey("Location", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.image_name
     class Meta:
         ordering = ['image_name']
+
+class Location(models.Model):
+    location = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.location
+    class Meta:
+        ordering = ['location']
+
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category
+    class Meta:
+        ordering = ['category']
