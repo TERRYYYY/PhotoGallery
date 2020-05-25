ff484f3cc4e06b9951a284cfa31040ef6f34636e
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-25 10:14:34 +0300

    connecting the application to postgres

--- tree/settings.py
+++ tree/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'tree.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tree',
+        'USER': 'terry',
+        'PASSWORD':'2002',
+    }
     }
 }
 
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
