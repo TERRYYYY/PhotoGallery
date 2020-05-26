91e3e962bfb46d834c6f19f28fb82f1c8cbd8763
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-26 18:13:38 +0300

    heroku

--- tree/settings.py
+++ tree/settings.py
@@ -17,7 +17,6 @@ from decouple import config, Csv
 
 MODE=config("MODE", default="dev")
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY', default=False)
+SECRET_KEY=config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
